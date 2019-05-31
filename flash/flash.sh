e7f312e712e0c1895112de115f53daef5d743e24
Emil Ringgaard <emil.ringgaard@hotmail.com>
2019-05-31 15:31:41 +0200

    dd

--- flash/flash.sh
+++ flash/flash.sh
@@ -3,4 +3,4 @@ cd build_target
 make
 cd ..
 cd flash
-avrdude -C /etc/avrdude.conf -v -p atmega328p -c arduino -P /dev/ttyACM0 -b 115200 -D -U flash:w:../build_target/avr_specific_files/toggle-led-cpp-atmega32.hex:i
+avrdude -C /etc/avrdude.conf -v -p atmega328p -c arduino -P /dev/ttyACM0 -b 115200 -D -U flash:w:../build_target/avr_code/Quadcopter-atmega32.hex:i
