a48088258501fe45e12f1ab0922e5a32fcad149b
Emil Ringgaard <emil.ringgaard@hotmail.com>
2019-06-19 21:43:22 +0200

    test code

--- avr_code/main.cpp
+++ avr_code/main.cpp
@@ -13,9 +13,7 @@ int main(void)
   while(1) //infinite loop
   {
     PORTB = 0xFF; //Turns ON All LEDs
-    _delay_ms(1000); //1 second delay
-    PORTB= 0x00; //Turns OFF All LEDs
-    _delay_ms(1000); //1 second delay
+
     
   }
 }
