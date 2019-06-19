65ef9e1f4b467bf988d16ac465283ccf7ab6db70
Emil Ringgaard <emil.ringgaard@hotmail.com>
2019-06-19 23:01:19 +0200

    d

--- flash/flash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ..
-cd build_target
-make
-cd ..
-cd flash
-avrdude -p m328p -c arduino -P /dev/ttyACM0 -b 115200 -v -U flash:w:../build_target/avr_code/Quadcopter-atmega32.hex
-#avrdude -C /etc/avrdude.conf -v -p atmega328p -c arduino -P /dev/ttyACM0 -b 115200 -D -U flash:w:../build_target/avr_code/Quadcopter-atmega32.hex:i
