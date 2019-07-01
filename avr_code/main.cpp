6d94a6451f9ee6b233253f705585b11e68f620c4
Emil Ringgaard <emil.ringgaard@hotmail.com>
2019-07-01 17:08:23 +0200

    Sending UART

--- avr_code/main.cpp
+++ avr_code/main.cpp
@@ -9,6 +9,28 @@
 #include "../FreeRTOS_avr/include/task.h"
 
 #include "communication/transport_layer/UART.hpp"
+#define BAUD 9600
+#define MYUBRR F_CPU/16/BAUD-1
+
+void USART_init(unsigned int ubrr) {
+    UBRR0H = (unsigned char)(ubrr >> 8);
+    UBRR0L = (unsigned char)ubrr;
+    UCSR0B = 1 << RXEN0 | 1 << TXEN0;
+    UCSR0C = 0 << USBS0 | 3 << UCSZ00;
+}
+
+void USART_transmit( unsigned char data ) {
+    while(!(UCSR0A & (1 << UDRE0)));
+    UDR0 = data;
+}
+
+void USART_transmit_string(const char *data) {
+    unsigned char c;
+    while(( c = *data++ )) {
+        USART_transmit(c);
+        _delay_ms(10);
+    }
+}
 
 void blinkLED(void* parameter)
 {
@@ -16,9 +38,10 @@ void blinkLED(void* parameter)
 	for (;;)
 	{
 		PORTB &= ~(1 << PB5);	// Turn LED on
-		vTaskDelay(250);	// Wait
+		vTaskDelay(1000);	// Wait
 		PORTB |= (1 << PB5);	// Turn LED off
 		vTaskDelay(1000);	// Wait
+		USART_transmit_string((const char *)"Hello world");
 	}
 }
 // MAIN PROGRAM
