0888dd9dbcd5f1cbf9d7495dc6d472e6c42f0c80
Emil Ringgaard <emil.ringgaard@hotmail.com>
2019-07-01 20:40:02 +0200

    staged

--- avr_code/main.cpp
+++ avr_code/main.cpp
@@ -3,9 +3,7 @@
 #endif
 
 #include <avr/io.h>
-#include <avr/delay.h>
 #include <stdio.h>
-#include <util/delay.h>
 #include "../FreeRTOS_avr/include/FreeRTOS.h"
 #include "../FreeRTOS_avr/include/task.h"
 
@@ -47,7 +45,7 @@ void vApplicationIdleHook(void)
 
 
 #define BUAD    9600
-#define BRC     ((F_CPU/8/BUAD) - 1)
+#define BRC     ((F_CPU/16/BUAD) - 1)
 #define TX_BUFFER_SIZE  128
  
 #include <util/delay.h>
