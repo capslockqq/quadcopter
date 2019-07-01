78449be2913fcfb53d8eb73d3c224cd79d27a9bf
Emil Ringgaard <emil.ringgaard@hotmail.com>
2019-07-01 18:04:28 +0200

    UART

--- avr_code/main.cpp
+++ avr_code/main.cpp
@@ -45,72 +45,45 @@ void vApplicationIdleHook(void)
 	// THIS RUNS WHILE NO OTHER TASK RUNS
 }
 
+#define BAUD 9600 
+#define MYUBRR    F_CPU/16/BAUD-1
 
-#define BUAD    9600
-#define BRC     ((F_CPU/16/BUAD) - 1)
-#define TX_BUFFER_SIZE  128
- 
-#include <util/delay.h>
-#include <avr/interrupt.h>
-char serialBuffer[TX_BUFFER_SIZE];
-uint8_t serialReadPos = 0;
-uint8_t serialWritePos = 0;
- 
-void appendSerial(char c);
-void serialWrite(const char *c);
- 
-int main(void)
-{   
-    UBRR0H = (BRC >> 8);
-    UBRR0L =  BRC;
-     
-    UCSR0B = (1 << TXEN0)  | (1 << TXCIE0);
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
-     
-    sei();
-         
-    while(1)
-    {
-		serialWrite((const char*)"HELLO\n\r\0");
-		_delay_ms(1000);
-    }
+void init_uart(unsigned short uValue  ){
+    // setting the baud rate  based on the datasheet 
+    UBRR0H =0x00;//(unsigned char)  ( uValue>> 8);  // 0x00 
+    UBRR0L =0x0C;//(unsigned char) uValue;  // 0x0C  
+    // enabling TX & RX 
+    UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+    UCSR0A = (1<<UDRE0)|(1<<U2X0);
+    UCSR0C =  (1 << UCSZ01) | (1 << UCSZ00);    // Set frame: 8data, 1 stop
+
 }
- 
-void appendSerial(char c)
+
+unsigned char USART_Receive( void )
 {
-    serialBuffer[serialWritePos] = c;
-    serialWritePos++;
-     
-    if(serialWritePos >= TX_BUFFER_SIZE)
-    {
-        serialWritePos = 0;
-    }
+    /* Wait for data to be received */
+    while ( !(UCSR0A & (1<<RXC0)) )
+    ;
+    /* Get and return received data from buffer */
+    return UDR0;
 }
- 
-void serialWrite(const char *c)
+void USART_Transmit( unsigned char data )
 {
-	int i = 0;
-	while(c[i] != '\0') {
-		appendSerial(c[i]);
-		i++;
-	}
-     
-    if(UCSR0A & (1 << UDRE0))
-    {
-        UDR0 = 0;
-    }
+    /* Wait for empty transmit buffer */
+    while ( !( UCSR0A & (1<<UDRE0)) )
+    ;
+    /* Put data into buffer, sends the data */
+    UDR0 = data;
 }
- 
-ISR(USART_TX_vect)
-{
-    if(serialReadPos != serialWritePos)
-    {
-        UDR0 = serialBuffer[serialReadPos];
-        serialReadPos++;
-         
-        if(serialReadPos >= TX_BUFFER_SIZE)
-        {
-            serialReadPos++;
-        }
+
+int main (){
+    init_uart(MYUBRR);
+    DDRD |= (1<< PD7);
+    for(;;){
+        PORTD ^= (1<< PD7);
+        USART_Transmit('x');
+        USART_Transmit('l');
+        _delay_ms(1000);
     }
+    return 0;
 }
