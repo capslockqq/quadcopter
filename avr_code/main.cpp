de6a18959dee862e19d6e9f87092873bfcc7264a
Emil Ringgaard <emil.ringgaard@hotmail.com>
2019-07-01 21:37:53 +0200

    UART in task

--- avr_code/main.cpp
+++ avr_code/main.cpp
@@ -9,10 +9,17 @@
 
 #include "communication/transport_layer/UART.hpp"
 
-
-#include <avr/io.h> 
-#include <util/delay.h>
-
+#define BUAD    9600
+#define BRC     ((F_CPU/8/BUAD) - 1)
+#define TX_BUFFER_SIZE  128
+ 
+char serialBuffer[TX_BUFFER_SIZE];
+uint8_t serialReadPos = 0;
+uint8_t serialWritePos = 0;
+ 
+void appendSerial(char c);
+void serialWrite(const char *c);
+ 
 void blinkLED(void* parameter)
 {
 	DDRB |= (1 << PB5);		// PB.5 as output
@@ -22,12 +29,17 @@ void blinkLED(void* parameter)
 		vTaskDelay(1000);	// Wait
 		PORTB |= (1 << PB5);	// Turn LED off
 		vTaskDelay(1000);	// Wait
+        appendSerial('1');
 
 	}
 }
 // MAIN PROGRAM
-/*int main(void)
+int main(void)
 {
+    UBRR0H = (BRC >> 8);
+    UBRR0L =  BRC;
+    UCSR0B = (1 << TXEN0)  | (1 << TXCIE0);
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 	// CREATE BLINKER TASK
 	xTaskCreate(blinkLED, "Print", configMINIMAL_STACK_SIZE, NULL, 7, NULL );
 
@@ -35,44 +47,13 @@ void blinkLED(void* parameter)
 	vTaskStartScheduler();
  
 	return 0;
-}*/
-
+}
 // IDLE TASK
 void vApplicationIdleHook(void)
 {
 	// THIS RUNS WHILE NO OTHER TASK RUNS
 }
 
-
-#define BUAD    9600
-#define BRC     ((F_CPU/8/BUAD) - 1)
-#define TX_BUFFER_SIZE  128
- 
-#include <util/delay.h>
-#include <avr/interrupt.h>
-char serialBuffer[TX_BUFFER_SIZE];
-uint8_t serialReadPos = 0;
-uint8_t serialWritePos = 0;
- 
-void appendSerial(char c);
-void serialWrite(const char *c);
- 
-int main(void)
-{   
-    UBRR0H = (BRC >> 8);
-    UBRR0L =  BRC;
-    UCSR0B = (1 << TXEN0)  | (1 << TXCIE0);
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
-     
-    sei();
-         
-    while(1)
-    {
-		appendSerial('1');
-		_delay_ms(1000);
-    }
-}
- 
 void appendSerial(char c)
 {
     while ( !( UCSR0A & (1<<UDRE0)));
