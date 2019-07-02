10e97d620ad0aba42426619029325a90cd01c81e
Emil Møller Ringgaard <emil.ringgaard@man-es.com>
2019-07-02 12:08:23 +0200

    Macro guard

--- FreeRTOS_tasks/tasks.cpp
+++ FreeRTOS_tasks/tasks.cpp
@@ -2,12 +2,16 @@
 int counter = 0;
 void *Test1(void *param) {
     while(1) {
+      
+      #ifdef PC
       counter++;
       std::cout << "Test1" << std::endl;
-      vTaskDelay(1000);
       if (counter >= 4) {
           vTaskEndScheduler();
           return NULL;
       }
+      #endif
+    vTaskDelay(1000);
+
    }
 }
