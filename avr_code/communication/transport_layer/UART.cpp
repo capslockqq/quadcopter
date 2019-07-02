439f9822ee92ac18b53ed09f3f214648da5f42b5
Emil Ringgaard <emil.ringgaard@hotmail.com>
2019-07-03 01:29:02 +0200

    UART

--- avr_code/communication/transport_layer/UART.cpp
+++ avr_code/communication/transport_layer/UART.cpp
@@ -5,14 +5,18 @@ UART::UART()
 }
 
 void UART::Send_Data(const char * data) {
-    int i = 0;
+    /*int i = 0;
     this->op_data.SetValue(data);
 	while(data[i] != '\0') {
 		while ( !( UCSR0A & (1<<UDRE0)));
         /* Put data into buffer, sends the data */
-        UDR0 = data[i];
+        /*UDR0 = data[i];
 		i++;
-	}
+	}*/
+    while ( !( UCSR0A & (1<<UDRE0)));
+    /* Put data into buffer, sends the data */
+    UDR0 = 'c';
+    
 }
 
 void UART::Receive_Data(const char * data) {
